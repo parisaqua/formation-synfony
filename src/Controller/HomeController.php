3a31568039fb4c349fca5e885dc029de1247f311
Fred <frederic@pmo.digital>
2019-02-15 20:50:05 +0100

    Premier commit, installation et page d'accueil

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class HomeController extends AbstractController {
+
+    /**
+     * @Route("/bonjour/{prenom}/age/{age}", name="hello")
+     * @Route("/salut", name="hello_base")
+     * @Route("/bonjour/{prenom}", name="hello_prenom")
+     *
+     * @return void
+     */
+    public function hello($prenom ="anonyme", $age=0){
+
+        return $this->render(
+            'hello.html.twig',
+            [
+                'prenom' => $prenom,
+                'age' => $age
+            ]
+            );
+    }
+
+    /**
+     * @Route("/", name="homepage")
+     *
+     */
+    public function home(){
+        $prenoms = ["Lior" => 31, "Joseph" => 12, "Anne" =>55];
+
+        return $this->render(
+            'home.html.twig', 
+            [
+                'title' => "Au revoir !",
+                'age' => 12,
+                'tableau' => $prenoms
+
+            ]
+        );
+    }
+
+}
+
+
+?>
